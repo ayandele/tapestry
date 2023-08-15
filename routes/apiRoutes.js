a8b1e90910655036da13c2173361853b50807fea
ayandele <adele.ayansola@yahoo.com>
2023-08-15 16:40:03 -0400

    removed duplicate code in apiRouter file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,6 +5,8 @@ const {
   createUser,
   updateUser,
   deleteUser,
+  addFriend,
+  removeFriend,
 } = require('../controllers/user-controller');
 
 const {
@@ -22,29 +24,6 @@ router.route('/users/:userId').get(getUserById).put(updateUser).delete(deleteUse
 // Define thought routes
 router.route('/thoughts').get(getAllThoughts).post(createThought);
 router.route('/thoughts/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
-
-module.exports = router;
-const router = require('express').Router();
-const {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend,
-} = require('../controllers/user-controller');
-
-// Routes for /api/users
-router.route('/users')
-  .get(getAllUsers)       // Get all users
-  .post(createUser);      // Create a new user
-
-// Routes for /api/users/:userId
-router.route('/users/:userId')
-  .get(getUserById)      // Get a user by ID
-  .put(updateUser)       // Update a user by ID
-  .delete(deleteUser);   // Delete a user by ID
 
 // Routes for adding and removing friends
 router.route('/users/:userId/friends/:friendId')
